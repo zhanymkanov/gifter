ddcc36f9b57a5185a56e65264980e5234831b70d
n1EzeR <erasylz@gmail.com>
2020-07-10 20:57:49 +0600

    Append slash to URLs

--- tests/apps/product/api/test_views.py
+++ tests/apps/product/api/test_views.py
@@ -16,7 +16,7 @@ def test_category_products_list(client: APIClient, category: Category) -> None:
     batch_size = 3
     ProductFactory.create_batch(batch_size, categories=(category,))
 
-    resp = client.get(f"/categories/{category.slug}")
+    resp = client.get(f"/categories/{category.slug}/")
 
     assert resp.status_code == 200
     assert len(resp.data) == batch_size
