a62e76ac44efd4ae760cd6de089e29d1a7605eca
n1EzeR <erasylz@gmail.com>
2020-07-11 15:21:47 +0600

    Add Category NOT_EXISTS handler

--- apps/gift/views.py
+++ apps/gift/views.py
@@ -1,6 +1,8 @@
 from rest_framework import permissions
+from rest_framework.exceptions import NotFound
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from ..constants import ErrorMessage
 from .models import Category, Gift
 from .serializers import CategoryListSerializer, GiftListSerializer
 
@@ -16,7 +18,11 @@ class CategoryGiftsList(ListAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
-        category = Category.objects.get(slug=self.kwargs["slug"])
+        try:
+            category = Category.objects.get(slug=self.kwargs["slug"])
+        except Category.DoesNotExist:
+            raise NotFound(detail=ErrorMessage.CATEGORY_DOES_NOT_EXIST)
+
         return Gift.objects.filter(categories__id=category.id, is_active=True)
 
 
--- tests/apps/gift/test_views.py
+++ tests/apps/gift/test_views.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 
+from apps.constants import ErrorMessage
 from apps.gift.models import Category, Gift
 from tests.factories import GiftFactory
 
@@ -20,6 +21,22 @@ def test_category_products_list(client: APIClient, category: Category) -> None:
 
     assert resp.status_code == 200
     assert len(resp.data) == batch_size
+
+
+@pytest.mark.django_db
+def test_category_products_not_exists(client: APIClient) -> None:
+    resp = client.get("/categories/NOT-EXISTS/")
+    resp_json = resp.json()
+
+    assert resp.status_code == 404
+    assert resp_json["detail"] == ErrorMessage.CATEGORY_DOES_NOT_EXIST
+
+
+@pytest.mark.django_db
+def test_gift_details(client: APIClient, gift: Gift) -> None:
+    resp = client.get(f"/gifts/{gift.slug}/")
+
+    assert resp.status_code == 200
 
 
 @pytest.mark.django_db
