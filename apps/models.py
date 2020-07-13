2a37f1cfaee1dfbc099bc2951216e98e86f9a374
n1EzeR <erasylz@gmail.com>
2020-07-14 00:33:46 +0600

    Init Order Model

--- apps/models.py
+++ apps/models.py
@@ -7,15 +7,15 @@ class ContactsModel(models.Model):
         abstract = True
 
     contact_name = models.CharField(max_length=64)
-    contact_phone = PhoneNumberField(db_index=True)
-    city = models.CharField()
-    shipping_address = models.CharField(blank=True)
+    contact_phone = PhoneNumberField(max_length=64, db_index=True)
+    city = models.CharField(max_length=64)
+    shipping_address = models.CharField(max_length=256, blank=True)
     email = models.EmailField(db_index=True)
 
 
 class SeoModel(models.Model):
-    seo_title = models.CharField(max_length=70, blank=True, null=True)
-    seo_description = models.CharField(max_length=300, blank=True, null=True)
-
     class Meta:
         abstract = True
+
+    seo_title = models.CharField(max_length=70, blank=True, null=True)
+    seo_description = models.CharField(max_length=300, blank=True, null=True)
--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "apps.gift",
-    "apps.seo",
+    "apps.order",
     "apps.vendor",
 ]
 
