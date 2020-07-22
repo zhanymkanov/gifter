080027348a30b4a3b4c234de5271a5c4b03cbef7
n1EzeR <erasylz@gmail.com>
2020-07-22 18:20:18 +0600

    Remove users init in orders
    Authentication will be rewritten with JWT, so the current version is irrelevant and can be skipped for now

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,6 +1,5 @@
-from rest_framework import permissions, status
+from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
-from rest_framework.response import Response
 
 from .serializers import OrderCreateSerializer
 
@@ -8,15 +7,3 @@ from .serializers import OrderCreateSerializer
 class OrderCreate(CreateAPIView):
     serializer_class = OrderCreateSerializer
     permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-        serializer = OrderCreateSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if request.user.is_authenticated:
-            serializer.save(user=request.user)
-        else:
-            serializer.save()
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
